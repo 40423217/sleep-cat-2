c2d90c26da917fecacfb79772d405737fe8ea05c
40423227 <jackson860711@gmail.com>
2016-01-17 22:31:16 +0800

    8afbuac

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -7,7 +7,7 @@ from __future__ import unicode_literals
 
 import os
 
-AUTHOR = '40423212'
+AUTHOR = '40423217'
 SITENAME = '2015FALL 40423217 CPB作業'
 SITEURL = ''
 
@@ -27,7 +27,7 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
          ('Python', 'http://python.org/'),('2015作業主頁', '../../'),
-         ('倉儲', 'http://40423217.github.io/sleep-cat-2/'))
+         ('40423217 Github', 'http://40423217.github.io/cpbhw/'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
